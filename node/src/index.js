80b5e84f3b99a0507fb345de14b5575fcc2117b1
Jaime Nanez <caicedojaime03z@gmail.com>
2023-05-10 15:30:47 -0500

    inicio proyecto

---
+++ node/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//routes
+app.get("/", (req, res)=> {
+    res.send("welcome to my api")
+})
+
+//conectar mongo con mi app
+mongoose.connect(
+    process.env.MONGODB_URI
+).then(()=> console.log("Estas conectado a Mongo Alas")).catch((error) => console.error(error));
+
+
+app.listen(port, ()=> console.log("Server is runnig on port ", port))
